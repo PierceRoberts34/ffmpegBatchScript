a6a2196f0d9d050d5faf0b61930fc65221f76628
Pierce Roberts <134012559+PierceRoberts34@users.noreply.github.com>
2024-02-15 20:29:28 -0500

    Add files via upload

---
+++ ffmpegBatch.py
@@ -0,0 +1,38 @@
+import os
+import tomllib
+
+with open('config.toml', 'rb') as f:
+    data = tomllib.load(f)
+print(data)
+
+# Convert video settings from config file into ffmpeg
+def videoSettings(filename):
+    string = f'ffmpeg -i \"{filename}\" -vf scale={data['resolution']} -crf {data['quality']} -preset {data['preset']}'
+    if data['fastStart']:
+        string += ' -movflags +faststart'
+    if data['copyAudio']:
+        string += ' -c:a copy'
+    string += f' \"1080 {filename}\"'
+    return string
+
+# Find all files in working directory
+dir = os.getcwd()
+files = os.listdir(dir)
+check = 0
+
+# Find all video files to be reencoded and verify ffmpeg exists
+videolist = []
+for filename in files:
+    if filename == 'ffmpeg.exe':
+        check += 1
+    if filename.endswith(('.mp4', '.mov', '.MOV', '.avi', '.mpg')):
+        print('video found')
+        videolist.append(filename)
+if check == 0:
+    print('ffmpeg not found')
+    exit()
+
+for video in videolist:
+    string = videoSettings(video)
+    os.system(string)
+
